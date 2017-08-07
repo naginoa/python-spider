e8462ba50cf5426760e25e9b80b8a2a4c137ec5a
任欣源 <245735349@qq.com>
2017-08-07 20:40:03 +0800

    分词结果加百度爬取

--- 中国美术家网爬取所有艺术家信息  5000多条/fenci.py
+++ 中国美术家网爬取所有艺术家信息  5000多条/fenci.py
@@ -1,4 +1,5 @@
 import jieba.analyse
+import requests
 
 
 def getkeystr(content):
@@ -11,6 +12,19 @@ def getkeystr(content):
     return keystr
 
 
-content = '''李聿仁 :李聿仁，专业画家，号晟煜，昊宸，纳海阁主人。山东淄博人，大学学历，现居北京。  中国长城书画家协会理事，中国美术家协会河北省分会会员，中国收藏家协会会员，山东淄博职业学院艺术设计系大写意花鸟画客座教授。'''
-a = getkeystr(content)
-print(a)
+def getHtmltext(keystr, kv):
+    url = "https://www.baidu.com/s?wd=" + keystr
+    try:
+        r = requests.get(url, headers = kv)
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return "产生异常"
+
+
+if __name__=="__main__":
+    content = '''李聿仁 :李聿仁，专业画家，号晟煜，昊宸，纳海阁主人。山东淄博人，大学学历，现居北京。  中国长城书画家协会理事，中国美术家协会河北省分会会员，中国收藏家协会会员，山东淄博职业学院艺术设计系大写意花鸟画客座教授。'''
+    kv = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36 LBBROWSER'}
+    a = getkeystr(content)
+    b = getHtmltext(a, kv)
+    print(b)
