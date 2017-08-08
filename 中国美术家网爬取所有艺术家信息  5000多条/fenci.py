0c06faa395c59f91a949b2b4ad1ffaec0155ca84
任欣源 <245735349@qq.com>
2017-08-08 14:48:03 +0800

    所有美术家的关键词
    太慢了 600多k处理了30分钟

--- 中国美术家网爬取所有艺术家信息  5000多条/fenci.py
+++ 中国美术家网爬取所有艺术家信息  5000多条/fenci.py
@@ -1,14 +1,15 @@
+# -*- coding: utf-8 -*-
 import jieba.analyse
 import requests
 
 
 def getkeystr(content):
-    sep = jieba.analyse.extract_tags(content, topK=10, withWeight=True)
+    sep = jieba.analyse.extract_tags(content, topK=10, withWeight=True, allowPOS=('np' ,'nr','n'))
     keylist = []
     for a, b in sep:
         # if ord(a[0]) > 127:   #去掉英文
         keylist.append(a)
-    keystr = ''.join(keylist)
+    keystr = ' '.join(keylist)
     return keystr
 
 
@@ -22,9 +23,30 @@ def getHtmltext(keystr, kv):
         return "产生异常"
 
 
+def getcontentlist(filename):
+    with open('file.txt', encoding='utf-8') as f:
+        text = f.read()  # list 存放读取的text  list2存放清洗后的text
+        alllist2 = []
+        alllist = text.split(':')  # 按照冒号分组
+        for sub in alllist:
+            sub2 = sub[:-5]  # 清洗
+            alllist2.append(sub2)
+        b = 0
+        for a in alllist2:  # 清洗空项
+            if a == '':
+                b += 1
+        flag = 1
+        while flag <= b:
+            alllist2.remove('')
+            flag += 1
+    return alllist2
+
+
 if __name__=="__main__":
-    content = '''李聿仁 :李聿仁，专业画家，号晟煜，昊宸，纳海阁主人。山东淄博人，大学学历，现居北京。  中国长城书画家协会理事，中国美术家协会河北省分会会员，中国收藏家协会会员，山东淄博职业学院艺术设计系大写意花鸟画客座教授。'''
     kv = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36 LBBROWSER'}
-    a = getkeystr(content)
-    b = getHtmltext(a, kv)
-    print(b)
+    filename = 'file.txt'
+    contentlist = getcontentlist(filename)
+    for content in contentlist:
+        keystr = getkeystr(content)
+        htmltext = getHtmltext(keystr, kv)
+        print(keystr)
