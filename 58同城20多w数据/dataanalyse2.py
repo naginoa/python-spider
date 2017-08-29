c2b6c2c630515b7fea6ee32b78e787474f3c8d7e
任欣源 <245735349@qq.com>
2017-08-29 22:54:38 +0800

    已完成58同城价格分析

--- 58同城20多w数据/dataanalyse2.py
+++ 58同城20多w数据/dataanalyse2.py
@@ -1,7 +1,9 @@
+http: // www.cnblogs.com / TTyb / p / 6051366.
+html
 import pymongo
 import charts
 
-client = pymongo.MongoClient('localhost',27017)
+client = pymongo.MongoClient('localhost', 27017)
 ceshi = client['copy']
 item_info = ceshi['item_info']
 
@@ -15,24 +17,51 @@ item_info = ceshi['item_info']
             pass
 '''
 
-calc = 0
-calc2 = 0
-calc3 = 0
-for i in item_info.find():
-    if '面议' not in i['price']:
-        if '万' not in i['price']:
-            if int(i['price']) > 2500 and int(i['price']) < 3000:
-                calc += 1
-        else:
-            calc2 += 1
-print(calc, calc2)
+re_list = []
 
-def gen_data():
+
+def gen_data(pre, after, re_list):
     calc = 0
     for i in item_info.find():
-    if '面议' not in i['price']:
-        if '万' not in i['price']:
-            if int(i['price']) < 100:
-                calc += 1
-            if int(i['price']) > 2500 and int(i['price']) < 3000:
-                calc += 1
+        if '面议' not in i['price']:
+            if '万' not in i['price']:
+                if int(i['price']) > pre and int(i['price']) < after:
+                    calc += 1
+    sub_list = [str(pre) + '-' + str(after), calc]
+    re_list.append(sub_list)
+
+
+key_list = [[0, 100], [100, 500], [500, 1000], [1000, 1500], [1500, 2000], [2000, 2500], [2500, 3000], [3000, 3500],
+            [3500, 4000], [4000, 4500], [4500, 5000]]
+
+
+def final_data(key_list, re_list):
+    calc = 0
+    for key in key_list:
+        gen_data(key[0], key[1], re_list)
+        gen_data(5000, 10000, re_list)
+    for i in item_info.find():
+        if '万' in i['price']:
+            calc += 1
+    re_list.append(['万元以上', calc])
+    calc = 0
+    for i in item_info.find():
+        if '面议' in i['price']:
+            calc += 1
+    re_list.append(['面议', calc])
+
+
+final_data(key_list, re_list)
+print(re_list)
+
+options = {
+    'charts': {'zoomType': 'xy'},
+    'title': {'text': '58北京地区物品价格对比'},
+    'subtitle': {'text': '来自风平浪静的明天'}
+}
+series = [{
+    'type': 'pie',
+    'name': 'Browser share',
+    'data': [data for data in re_list],
+}]
+charts.plot(series, show='inline', options=options)
